73116aa5ef60eecb1a7d20530ebe2ae6ea89872a
Leon Guilherme Lopes da Silva <leon.silva@flexpag.local>
2022-09-28 14:37:33 -0300

    create: tela login e novo usuario

---
+++ src/app/components/home/home-routing.module.ts
@@ -0,0 +1,28 @@
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponent } from './new-user/new-user.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'novousuario',
+        component: NewUserComponent,
+      }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomeRoutingModule {}
---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,17 @@
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  declarations: [LoginComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
