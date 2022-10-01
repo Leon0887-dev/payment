a02830c30c409181a51ee0185f9f37a121d651de
Leon Guilherme Lopes da Silva <leon.silva@flexpag.local>
2022-10-01 20:50:37 -0300

    create: guarda das rotas home e agendamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
@@ -11,11 +13,16 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () =>
       import('./components/home/home.module').then((m) => m.HomeModule),
+    canLoad:[LoginGuard]
   },
   {
     path: 'agendamentos',
-    loadChildren: () => import('./components/payment-scheduling/payment-scheduling.module').then(m => m.PaymentSchedulingModule)
-  }
+    loadChildren: () =>
+      import('./components/payment-scheduling/payment-scheduling.module').then(
+        (m) => m.PaymentSchedulingModule
+      ),
+    canLoad: [AutenticacaoGuard]
+  },
 ];
 
 @NgModule({
