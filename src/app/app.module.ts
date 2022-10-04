7e17d4b0a1de6951182d42c00f9bce6ad3f0a74a
leon guilherme <leonlopes1106@gmail.com>
2022-10-04 14:02:19 -0300

    feat:  importado modulos locale_id e ngxmaskMOdule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 
 
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgxMaskModule, IConfig } from 'ngx-mask'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+ import localePT from '@angular/common/locales/pt'
+ registerLocaleData(localePT);
 
 
-
-
+ export  const  options : Partial < null | IConfig >  |  ( ( )  =>  Partial < IConfig > )  =  null ;
 
 
 @NgModule({
@@ -21,10 +24,12 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgxMaskModule.forRoot()
+    
    
    
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-br'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
