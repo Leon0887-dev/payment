961dcc39c81c9ae92351336fceaa4d5f470a1287
leon guilherme <leonlopes1106@gmail.com>
2022-10-04 14:08:30 -0300

    feat: alterada rota do endpoint

--- src/app/components/home/service/new-user-service.service.ts
+++ src/app/components/home/service/new-user-service.service.ts
@@ -11,11 +11,11 @@ export class NewUserServiceService {
   constructor(private http: HttpClient) { }
 
   newRegister(newUser: NewUser){
-    return this.http.post('http://localhost:3000/user/signup', newUser)
+    return this.http.post('http://localhost:8080/user', newUser)
   }
 
 
   checkExistingUser(nameUser: string){
-    return this.http.get(`http://localhost:3000/user/exists/${nameUser}`)
+    return this.http.get(`http://localhost:8080/user/${nameUser}`)
   }
 }
