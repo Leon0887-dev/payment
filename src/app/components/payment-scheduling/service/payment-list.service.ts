8706b10928a5183677fbfa7f0a517f6b0c18e63e
leon guilherme <leonlopes1106@gmail.com>
2022-10-05 10:44:05 -0300

    feat: criado getters e setters para comunicação de componentes

--- src/app/components/payment-scheduling/service/payment-list.service.ts
+++ src/app/components/payment-scheduling/service/payment-list.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CreatePayment } from '../create-payment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentListService {
+  set(scheduleList: any) {
+    throw new Error('Method not implemented.');
+  }
 
-  
+  public _paymentList!: any[]
+
+  get paymentList():any {
+    return this._paymentList;
+  }
+
+  set paymentList(value: any){
+    this._paymentList = value;
+  }
 
   constructor() { }
 }
