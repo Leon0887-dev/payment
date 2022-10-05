97795123baf2ecc6cd478c8de5ed5bc30a9e3e8c
leon guilherme <leonlopes1106@gmail.com>
2022-10-05 11:43:33 -0300

    feat: criado novo componente para pesquisar os status

---
+++ src/app/components/payment-scheduling/appointment-list/search-status/search-status.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchStatusComponent } from './search-status.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchStatusComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SearchStatusModule { }
