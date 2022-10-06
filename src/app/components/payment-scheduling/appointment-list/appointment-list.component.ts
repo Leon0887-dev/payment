3e270d592d435662d8e2d9131f40ae26ba708d84
leon guilherme <leonlopes1106@gmail.com>
2022-10-06 14:08:51 -0300

    feat: adicionado ao componente botões de filtro e lógica da renderização

--- src/app/components/payment-scheduling/appointment-list/appointment-list.component.ts
+++ src/app/components/payment-scheduling/appointment-list/appointment-list.component.ts
@@ -2,27 +2,41 @@ import { PaymentServiceService } from './../service/payment-service.service';
 import { PaymentListService } from './../service/payment-list.service';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.scss']
+  styleUrls: ['./appointment-list.component.scss'],
 })
 export class AppointmentListComponent implements OnInit {
-
+  paymentAllList!: any;
   paymentList!: any;
 
-  constructor( private paymentService: PaymentServiceService ) {
-   }
+  constructor(private paymentService: PaymentServiceService) {}
 
   ngOnInit(): void {
-      this.paymentService.readPayments().subscribe(res =>{
-        this.paymentList = res.content
-      })
-    }
-    
+    this.getAllPayments();
   }
 
 
 
+  getAllPayments() {
+    this.paymentService.readPayments().subscribe((res) => {
+      this.paymentAllList = res.content;
+      this.paymentList = this.paymentAllList;
+    });
+  }
 
+  getPaid() {
+    this.paymentList = this.paymentAllList
+    this.paymentList = this.paymentList.filter(
+      (item: any) => item.status === 'PAID'
+    );
+  }
+
+  getPending() {
+    this.paymentList = this.paymentAllList
+    this.paymentList = this.paymentList.filter(
+      (item: any) => item.status === 'PENDING'
+    );
+  }
+}
